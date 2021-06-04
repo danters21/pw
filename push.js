a550daaf012f16ed99a1a075fef23bbfe7606210
Dante Sarigumba <consult-dsarigumba@cerberus.com>
2021-06-04 11:46:26 -0400

    www

--- push.js
+++ push.js
@@ -6,4 +6,10 @@ var vapidKeys = {
     privateKey: 'U-jj5JporIauYystguZmGRKBuy7zCTABCKXgwxG8pRI'
   };
 
-console.log(vapidKeys);
+//console.log(vapidKeys);
+
+push.setVapidDetails('mailto:testbla@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
+
+let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/dOJSugPiYI8:APA91bGoY2byMAr62zFwnsQpAgukRn1ulFMUUMHo_zbbo7tXcIjKiKLLSyj0AQcRTO-Cuiqu5F-GNIH9ZKYaCgJZvZ33lzmEhm6apI2Vsur6N_e8IGVlOk0cQKM3pd6fuxGBzr13HxfG","expirationTime":null,"keys":{"p256dh":"BCwP9Vc2fGE_VFDltKljY8B_RVrY9TeWDFweeErK_XCvRJaJjWv5hcseBQV8wR55JWDicVJYLDC4BTrozsUfUcY","auth":"gSs1sdf-N8xC5WSBY1g40Q"}};
+
+push.sendNotification(sub);
--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
-self.addEventListener('push', ()=> {
-    self.ServiceWorkerRegistration.sendNotification('test message', {});
-});
+self.addEventListener('push', function(e) {
+    var options = {
+        body: 'This notification was generated from a push!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            { action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'},
+                { action: 'close', title: 'Close',
+                icon: 'images/xmark.png'},
+        ]
+    };
+    e.waitUntil(
+        self.ServiceWorkerRegistration.showNotification('New vehicles on your watchlist', options)
+    );
+    }
+);
 
 // git remote add origin git_url_here
