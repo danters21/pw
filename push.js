75d64eea0e53a40c13c10c451c4d79c5afd4e69e
Dante Sarigumba <consult-dsarigumba@cerberus.com>
2021-06-07 11:53:59 -0400

    testing2

--- push.js
+++ push.js
@@ -14,5 +14,6 @@ let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/dOJSugPiYI8:APA91bGoY2b
 
 let sub_win = {"endpoint":"https://bn3p.notify.windows.com/w/?token=BQYAAAAfvdeShmbLUEBYcipLQ8EkSROfpDgAQSjtrA8z%2bo12CZYwUDOUZuDr2QKVHiKWq66%2fFjfEngDfWTtj3sCDqg0UKlXOQ5xiUW9ugjEj0x6e4Ox99g35ytKqACbUh%2bVisBVh%2fc4zTymPTg%2fTvVgfjoPoqTbal%2bf3lr0%2b8uoAylYE0wgCWs7JqKQh7FKa2SB1mG1Xrh%2bUayviX9%2fsaHXSVEYwPyyfJH7Flgoo5toS4UK2LVgKFuE%2fMnh9ePGZV1UbhBtYRD3BjWCya7pQ6p2Mq6EDJiCeD6qi78dOKMqqt9zC24lnNoKNB%2b8p9kv%2b8y06RMnCI4yzi%2bkrqnRLPirtW3iUEEi1LeEJLAPbHT5eGEhuCQ%3d%3d","expirationTime":null,"keys":{"p256dh":"BKIZkuyaEayk74hX1lnlFUni_APlJQl8Kk3AmjUWbLsho_pLxWTDjJLuF1YyKJiWm5KDBY4tDSDEIMVXKRU7Ggw","auth":"FugplQWL98_sS8lsXa-d6g"}};
 
-push.sendNotification(sub, 'This is a test');
-//push.sendNotification(sub_win, 'This is a test');
+var payload = JSON.stringify({message: 'Testing message', url: 'https://www.google.com'});
+push.sendNotification(sub, payload);
+//push.sendNotification(sub_win, payload);
--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 self.addEventListener('push', function(e) {
+    var payload = JSON.parse(e.data.text());
     var options = {
-        body: 'Data from push: ' + e.data.text(),
+        body: 'Data from push: ' + payload.message,
         icon: 'images/example.png',
         vibrate: [100, 50, 100],
         data: {
             dateOfArrival: Date.now(),
-            primaryKey: '2'
+            primaryKey: '2',
+            url: payload.url
         },
         actions: [
             { action: 'explore', title: 'Explore this new world',
@@ -14,11 +16,17 @@ self.addEventListener('push', function(e) {
                 icon: 'images/xmark.png'},
         ]
     };
-    e.waitUntil(
+    e.waitUntil(        
         self.registration.showNotification('Updated new vehicles', options)
     );
     console.log("force change");
   }
 );
 
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+      clients.openWindow(event.notification.data.url)
+    );
+  });
 // git remote add origin git_url_here
